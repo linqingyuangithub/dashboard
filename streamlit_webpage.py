b9b0734f976b3d29213be9dc9340266caead4783
LinqingYuan <41339410+linqingyuangithub@users.noreply.github.com>
2023-10-12 14:39:19 -0400

    Update streamlit_webpage.py

--- streamlit_webpage.py
+++ streamlit_webpage.py
@@ -7,13 +7,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import random
-from streamlit_d3_demo import d3_line
-import altair as alt
-import os
-import matplotlib.pyplot as plt
-#from datetime import datetime
-from datetime import date
+
 
 st.title('Have a good day')
 st.title('I love cats :blue[cool] :sunglasses:')
