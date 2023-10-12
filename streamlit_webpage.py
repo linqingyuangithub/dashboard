05f6a01c66d44ab84fb749ebd2b9530df984dc60
LinqingYuan <linqingyuan0927@gmail.com>
2023-10-12 11:23:44 -0400

    app

---
+++ streamlit_webpage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+st.title('Have a good day')
+st.title('I love cats :blue[cool] :sunglasses:')
+
+
+# In[ ]:
+
+
+
+
