bfc7e0bced1a575223edbb31cf8fc4228107e480
LinqingYuan <41339410+linqingyuangithub@users.noreply.github.com>
2023-10-12 14:40:16 -0400

    Update streamlit_webpage.py

--- streamlit_webpage.py
+++ streamlit_webpage.py
@@ -12,7 +12,8 @@ import streamlit as st
 st.title('Have a good day')
 st.title('I love cats :blue[cool] :sunglasses:')
 
-
+combined_df = pd.read_csv('./OneDrive - Delta Enterprise Corp/combined inv/combined_df.csv', dtype={"Date": str})
+combined_df
 # In[3]:
 
 
